e54b2dbf1a4aba609922399b31091745dc92cbee
Stefano <s.cislaghi@gmail.com>
2021-02-10 12:01:32 +0000

    Correct a missing comment.
    Line one is probably a comment which has been erroneously left trimmed.

--- LTLA_public/lib/justgage-1.2.2.1/justgage.js
+++ LTLA_public/lib/justgage-1.2.2.1/justgage.js
@@ -1,4 +1,4 @@
-rflow.com/questions/2901102/how-to-print-a-number-with-commas-as-thousands-separators-in-javascript */
+/* https://stackoverflow.com/questions/2901102/how-to-print-a-number-with-commas-as-thousands-separators-in-javascript */
 function formatNumber(x) {
   var parts = x.toString().split(".");
   parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
